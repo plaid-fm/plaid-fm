639efa395154872f807ffc543e8db837705cdb75
Taylor Reis <treis@plaid.com>
2019-07-10 21:07:31 -0700

    Good enough

--- src/Content/CatalogContainer/index.js
+++ src/Content/CatalogContainer/index.js
@@ -6,12 +6,9 @@ const CatalogContainer = () => {
   return (
     <div className="catalog">
       <h3>Stream from the Catalog</h3>
-      Hymns sang by Angels (kiddin')!.
-      <br />
-      Explore our Channel Catalog for tunes for just the right Mood.
-      <br />
-      <strong>Go to Catalog > </strong>
-      <br />
+      <p>Hymns sang by Angels (kiddin')! Explore our</p>
+      <p>Channel Catalog for tunes for just the right Mood.</p>
+      <strong>Check out the catalog</strong>
       <div className="img-wrapper">
         <img src="https://www.yourlocalplumber.co.uk/wp-content/uploads/2016/12/greensquare.jpg" />
         <img src="https://www.yourlocalplumber.co.uk/wp-content/uploads/2016/12/greensquare.jpg" />
