84d034ba2ce00abe92e4d6ea54e52744c5341f53
Taylor Reis <treis@plaid.com>
2019-07-10 18:03:03 -0700

    Add link

--- src/Content/ListenContainer/index.js
+++ src/Content/ListenContainer/index.js
@@ -4,18 +4,24 @@ import './style.css';
 
 const ListenContainer = () => {
   return (
-  <div className='listen'>
+    <div className="listen">
       <h2>Listen live with JQBX</h2>
       Listen live with other Plaids at the same time.
       <br />
       Requires Spotify Premium.
       <br />
-      <strong>Go to JQBX > </strong>
+      <a
+        href="https://app.jqbx.fm/room/5d262aa339a99000c4ebe1d4"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <strong>Go to JQBX > </strong>
+      </a>
       <br />
       <div className="img-wrapper">
-        <img src="https://www.yourlocalplumber.co.uk/wp-content/uploads/2016/12/greensquare.jpg"></img>
+        <img src="https://www.yourlocalplumber.co.uk/wp-content/uploads/2016/12/greensquare.jpg" />
       </div>
-  </div>
+    </div>
   );
 };
 
