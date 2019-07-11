8528b479b48ab7a2cd13ea0ee55523f2299c1a20
Luigi Sambuy <lsambuy@plaid.com>
2019-07-10 17:54:14 -0700

    styled pictures in content section

--- src/Content/CatalogContainer/index.js
+++ src/Content/CatalogContainer/index.js
@@ -6,9 +6,21 @@ const CatalogContainer = () => {
   return (
     <div className='catalog'>
       <h2>Stream from the Catalog</h2>
+      Hymns sang by Angels (kiddin')!.
+      <br />
       Explore our Channel Catalog for tunes for just the right Mood.
       <br />
       <strong>Go to Catalog > </strong>
+      <br />
+      <div className="img-wrapper">
+        <img src="https://www.yourlocalplumber.co.uk/wp-content/uploads/2016/12/greensquare.jpg"></img>
+        <img src="https://www.yourlocalplumber.co.uk/wp-content/uploads/2016/12/greensquare.jpg"></img>
+        <img src="https://www.yourlocalplumber.co.uk/wp-content/uploads/2016/12/greensquare.jpg"></img>
+        <img src="https://www.yourlocalplumber.co.uk/wp-content/uploads/2016/12/greensquare.jpg"></img>
+        <img src="https://www.yourlocalplumber.co.uk/wp-content/uploads/2016/12/greensquare.jpg"></img>
+        <img src="https://www.yourlocalplumber.co.uk/wp-content/uploads/2016/12/greensquare.jpg"></img>
+      </div>
+
 
     </div>
   );
--- src/Content/ListenContainer/index.js
+++ src/Content/ListenContainer/index.js
@@ -5,11 +5,16 @@ import './style.css';
 const ListenContainer = () => {
   return (
   <div className='listen'>
-    <h2>Listen live with JQBX</h2>
-    Listen live with other Plaids at the same time.
-    <br />
-    Requires Spotify Premium.
-    <strong>Go to JQBX > </strong>
+      <h2>Listen live with JQBX</h2>
+      Listen live with other Plaids at the same time.
+      <br />
+      Requires Spotify Premium.
+      <br />
+      <strong>Go to JQBX > </strong>
+      <br />
+      <div className="img-wrapper">
+        <img src="https://www.yourlocalplumber.co.uk/wp-content/uploads/2016/12/greensquare.jpg"></img>
+      </div>
   </div>
   );
 };
